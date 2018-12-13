260bd461a94d9e2ffcdc06d7b81d4ddb7b0d4a5d
LordU212 <44179637+LordU212@users.noreply.github.com>
2018-12-13 04:56:18 -0500

    Add files via upload

---
+++ finalproject/js/index.js
@@ -0,0 +1,8 @@
+
+       function seePrompt(){
+           var favorite = new String(window.prompt("Who is your favorite NBA Player?"));
+           document.getElementById("promptResp").value = "Your favorite player is: " + favorite;
+       }
+       
+
+
